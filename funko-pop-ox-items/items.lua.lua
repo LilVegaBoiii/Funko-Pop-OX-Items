9c3e021979f4b67892eb9e183cfd901ffd553266
LilVegaBoiii <127635959+LilVegaBoiii@users.noreply.github.com>
2023-06-02 12:15:27 -0400

    Add files via upload

---
+++ funko-pop-ox-items/items.lua.lua
@@ -0,0 +1,174 @@
+	['funkopika'] = {
+		label = 'Pikachu',
+		weight = 1,
+		stack = true,
+		close = false,
+	},
+
+	['funkosquirtle'] = {
+		label = 'Squirtle',
+		weight = 1,
+		stack = true,
+		close = false,
+	},
+
+	['funkobulbasaur'] = {
+		label = 'Bulbasaur',
+		weight = 1,
+		stack = true,
+		close = false,
+	},
+
+	['funkoeevee'] = {
+		label = 'Eevee',
+		weight = 1,
+		stack = true,
+		close = false,
+	},
+
+	['funkocharizard'] = {
+		label = 'Charizard',
+		weight = 1,
+		stack = true,
+		close = false,
+	},
+
+	['funkocharmander'] = {
+		label = 'Charmander',
+		weight = 1,
+		stack = true,
+		close = false,
+	},
+
+	['funkomewtwo'] = {
+		label = 'Mewtwo',
+		weight = 1,
+		stack = true,
+		close = false,
+	},
+
+	['funkoponyta'] = {
+		label = 'Ponyta',
+		weight = 1,
+		stack = true,
+		close = false,
+	},
+
+	['funkoflockedvulpix'] = {
+		label = 'Vulpix (Flocked)',
+		weight = 1,
+		stack = true,
+		close = false,
+	},
+
+	['funkoriachu'] = {
+		label = 'Riachu',
+		weight = 1,
+		stack = true,
+		close = false,
+	},
+
+	['funkom2flockedsdcc'] = {
+		label = 'SDCC Mewtwo (Flocked)',
+		weight = 1,
+		stack = true,
+		close = false,
+	},
+
+	['funkostitch'] = {
+		label = 'Stitch',
+		weight = 1,
+		stack = true,
+		close = false,
+	},
+
+	['funkoblacklightjack'] = {
+		label = 'Jack Skellington (Blacklight)',
+		weight = 1,
+		stack = true,
+		close = false,
+	},
+
+	['funkobuzz'] = {
+		label = 'Buzz Lightyear',
+		weight = 1,
+		stack = true,
+		close = false,
+	},
+
+	['funkosheriffwoody'] = {
+		label = 'Sheriff Woody',
+		weight = 1,
+		stack = true,
+		close = false,
+	},
+
+	['funkojakesully'] = {
+		label = 'Jake Sully',
+		weight = 1,
+		stack = true,
+		close = false,
+	},
+
+	['funkobuttstallion'] = {
+		label = 'Butt Stallion',
+		weight = 1,
+		stack = true,
+		close = false,
+	},
+
+	['funkotinytina'] = {
+		label = 'Tiny Tina',
+		weight = 1,
+		stack = true,
+		close = false,
+	},
+
+	['funkogoku'] = {
+		label = 'Goku',
+		weight = 1,
+		stack = true,
+		close = false,
+	},
+
+	['funkovegeta'] = {
+		label = 'Vegeta',
+		weight = 1,
+		stack = true,
+		close = false,
+	},
+
+	['funkopiccolo'] = {
+		label = 'Piccolo',
+		weight = 1,
+		stack = true,
+		close = false,
+	},
+
+	['funkonaruto'] = {
+		label = 'Naruto',
+		weight = 1,
+		stack = true,
+		close = false,
+	},
+
+	['funkoitachi'] = {
+		label = 'Itachi',
+		weight = 1,
+		stack = true,
+		close = false,
+	},
+
+	['funkokakashi'] = {
+		label = 'Kakashi',
+		weight = 1,
+		stack = true,
+		close = false,
+	},
+
+	['funkofreddyfunko'] = {
+		label = 'Freddy Funko as Rick (Glow in the Dark)',
+		weight = 1,
+		stack = true,
+		close = false,
+	}
